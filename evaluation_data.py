4914accc8c7726e68eec5ee1c9ae91f74de6d8b9
ryosei62 <uppu1229@gmail.com>
2025-11-14 10:52:58 +0900

    create evaluation_data

--- evaluation_data.py
+++ evaluation_data.py
@@ -1,12 +1,39 @@
 EVALUATION_SETS = [
-    {
-        "name": "TestSet1",
-        "query_index": 330,  # 例: 330
-        "ground_truth_indices": [390, 443],  # 例: [390, 443]
-    },
-    {
-        "name": "TestSet2",
-        "query_index": 481,
-        "ground_truth_indices": [445, 499, 755],
-    },
+    {"query_index": 30, "ground_truth_indices": [143,145]},
+    {"query_index": 106,"ground_truth_indices": [107]},
+    {"query_index": 140, "ground_truth_indices": [26,142,67]},
+    {"query_index": 31,"ground_truth_indices": [72,73]},
+    {"query_index": 90, "ground_truth_indices": [174,58]},
+    {"query_index": 117,"ground_truth_indices": [119]},
+    {"query_index": 57, "ground_truth_indices": [158,176]},
+    {"query_index": 52,"ground_truth_indices": [89]},
+    {"query_index": 27, "ground_truth_indices": [40]},
+    {"query_index": 144,"ground_truth_indices": [165]},
+    #10
+    {"query_index": 161, "ground_truth_indices": [201,250,180]},
+    {"query_index": 247,"ground_truth_indices": [246,250,240]},
+    {"query_index": 83, "ground_truth_indices": [182,87]},
+    {"query_index": 66,"ground_truth_indices": [232]},
+    {"query_index": 290, "ground_truth_indices": [286,272]},
+    {"query_index": 259,"ground_truth_indices": [272,290]},
+    {"query_index": 333, "ground_truth_indices": [370]},
+    {"query_index": 359,"ground_truth_indices": [252,396]},
+    {"query_index": 183, "ground_truth_indices": [184]},
+    {"query_index": 340,"ground_truth_indices": [240]},
+    #20
+    {"query_index": 440, "ground_truth_indices": [481]},
+    {"query_index": 506,"ground_truth_indices": [345]},
+    {"query_index": 232, "ground_truth_indices": [254,411]},
+    {"query_index": 508,"ground_truth_indices": [502, 356]},
+    {"query_index": 433, "ground_truth_indices": [414]},
+    {"query_index": 475,"ground_truth_indices": [142]},
+    {"query_index": 569, "ground_truth_indices": [310]},
+    {"query_index": 317,"ground_truth_indices": [591]},
+    {"query_index": 529, "ground_truth_indices": [698]},
+    {"query_index": 599,"ground_truth_indices": [356]},
+    #30
+    {"query_index": 671, "ground_truth_indices": [138]},
+    {"query_index": 683,"ground_truth_indices": [640,765,543]},
+    #40
+
 ]
